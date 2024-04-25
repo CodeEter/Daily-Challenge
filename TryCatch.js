e444628758d3bbca7bbe363641893e3cafab7ded
CodeEter <47411900+CodeEter@users.noreply.github.com>
2024-04-25 08:59:39 -0400

    Update TryCatch.js
    extending error

--- TryCatch.js
+++ TryCatch.js
@@ -1,12 +1,33 @@
-function func() {
-  try {
-    return 1;
-  } catch (err) {
-
-  } finally {
-    console.log('finally');
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.message = "ValidationError";
   }
 }
 
-console.log( func() );
+//Usage
+function readUser(json) {
+  let user = JSON.parse(json);
 
+  if (!user.age) {
+    throw new ValidationError("No field: age");
+  }
+  if (!user.name) {
+    throw new ValidationError("No field: name");
+  }
+  return user;
+}
+
+//Working example with try...catch
+
+try {
+  let user = readUser('{"age": 25}');
+} catch (err) {
+  if (err instanceof ValidationError) {
+    console.log("Invalid data: " + err.message);
+  } else if (err instanceof SyntaxError) {
+    console.log("JSON Syntax Error: " + err.message);
+  } else {
+    throw err;
+  }
+}
